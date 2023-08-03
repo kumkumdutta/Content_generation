9ae06f7564e6912dda6c12b8387d5279c56611b1
KumkumDutta <112754510+kumkumdutta@users.noreply.github.com>
2023-08-04 00:10:29 +0530

    nsjjdfjs

--- index.js
+++ index.js
@@ -45,7 +45,11 @@
 
 // app.listen(process.env.port,()=>{
 //     console.log(`server is running on port ${process.env.port}`)
+
+
 // })
+
+
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const cors = require("cors")
